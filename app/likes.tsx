e6b016abeb1eaa529ffa50169d9d1bdc1a662756
Avery King <averyking345@proton.me>
2025-08-23 12:24:20 -0500

    state added for like

--- app/likes.tsx
+++ app/likes.tsx
@@ -1,18 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function Likes({ post }: any) {
+  const [likesCount, setLikesCount] = useState(post.likes.length);
+
   const handleLikes = async () => {
     const supabase = createClientComponentClient();
     const {
       data: { user },
     } = await supabase.auth.getUser();
     if (user) {
-      await supabase
+      const { error } = await supabase
         .from("likes")
         .insert([{ user_id: user?.id, post_id: post.id }]);
+      if (!error) {
+        setLikesCount((c) => c + 1);
+      }
     }
   };
-  return <button onClick={handleLikes}>{post.likes.length} Likes</button>;
+  return <button onClick={handleLikes}>{likesCount} Likes</button>;
 }
